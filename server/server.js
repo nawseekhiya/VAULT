109a71bb4b619ff3da4b36e173b2d8c4007c1875
nawseekhiya <araj80317@gmail.com>
2024-11-07 10:11:22 +0530

    Completed

--- server/server.js
+++ server/server.js
@@ -2,15 +2,26 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept json data in the req.body (middleware)
 
 app.use("/api/products", productRoutes); //  using routes to call APIs
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
